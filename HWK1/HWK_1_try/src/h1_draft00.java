2706e6f987a750cb4b6f48638b499b162348fea7
Mengyao Ruan <ruanmiao@umich.edu>
2017-09-21 21:13:19 -0400

    begin strategy 3

--- HWK1/HWK_1_try/src/h1_draft00.java
+++ HWK1/HWK_1_try/src/h1_draft00.java
@@ -11,45 +11,52 @@ public class h1_draft00
 //	LEFT_DOWN,  6        LEFT_UP      7
 
     private static final int max_num_states = 1000000;
-    private static final int board_size = 64;
+    
     private static final int board_edge_size = 8;
+    private static final int board_size = board_edge_size * board_edge_size;
+
     private static final int start_x = 2;
     private static final int start_y = 3;
     private static final int num_cases = 8;
+    private static final int max_sol_count = 200;
     
     private static Random rn = new Random();
     private static boolean findSol = false;
 
     public static boolean[][] visited_state = new boolean[board_edge_size][board_edge_size];
+    public static int[][] fixed_degree = new int[board_edge_size][board_edge_size];
+    public static int[][] dynamics_degree = new int[board_edge_size][board_edge_size];
     public static int[] tour_x_trace = new int[board_size];
     public static int[] tour_y_trace = new int[board_size];
     
     // when the value of to_search_case >= 8, no more sol; it stores the un-explored case_ind 
     public static int[] to_search_cases = new int[board_size];
+    //public static boolean[][] valid_to_search_cases = new boolean[board_size][num_cases];
     
-    //public static int cur_m_ind = 0;
     public static int sol_count = 0;
-//    public static int cur_x = start_x;
-//    public static int cur_y = start_y;
 
+    
     public static void main(String[] args)
     {
     	visited_state[start_x][start_y] = true;
     	tour_x_trace[0] = start_x;
     	tour_y_trace[0] = start_y;
-    	//cur_m_ind = 0;
     	
+    	// initializeAllDegree();
     	resetAll();
      
     	String text = "KT: 8x8, strategy = 1, start = 2,3";
     	System.out.println(text);
 
     	sol_count = 0;
-    	runStrategy0(0);
+    	runStrategy2(0);
     	
         return ;
     }
 
+    
+    /////////////////////////// reset, initialization, prepare for new run //////////////////
+    
     public static void resetAll()
     {
     	for (int m_ind = 1; m_ind < board_size; m_ind++)
@@ -61,6 +68,10 @@ public class h1_draft00
 			tour_x_trace[m_ind] = -1;
 			tour_y_trace[m_ind] = -1;
 			to_search_cases[m_ind] = 0;
+			//for (int case_ind = 0; case_ind < num_cases; case_ind++)
+			//{
+			//	valid_to_search_cases[m_ind][case_ind] = true;				
+			//}
     	}
     	visited_state[start_x][start_y] = true;
     	tour_x_trace[0] = start_x;
@@ -68,41 +79,39 @@ public class h1_draft00
     	to_search_cases[0] = 0;
     	//cur_m_ind = 0;
     	findSol = false;
-    }
-    
-    // All the data from #reset_ind will be wiped, reset_ind >0;
-    public static void resetSol(int reset_ind)
-    {
-    	assert(reset_ind > 0);
-    	assert(reset_ind < board_size);
-    	for (int m_ind = reset_ind; m_ind < board_size; m_ind++)
-    	{    		
-    		int x_ind = tour_x_trace[m_ind];
-    		int y_ind = tour_y_trace[m_ind];
-    		
-    		if((tour_x_trace[m_ind]<0))
-    		{    			
-    			return ;
-    		}
-    		/*
-    		if(!(visited_state[x_ind][y_ind]) && (tour_x_trace[m_ind]<0))
-    		{
-    			return ;
-    		}
-
-    		 */
-    		
-    		visited_state[x_ind][y_ind] = false;    		
-    		tour_x_trace[m_ind] = -1;
-    		tour_y_trace[m_ind] = -1;
-    		to_search_cases[m_ind] = 0;
-    	}
     	
-    	//cur_m_ind = reset_ind - 1;
-    	visited_state[start_x][start_y] = true;
-    	tour_x_trace[0] = start_x;
-    	tour_y_trace[0] = start_y;
+    	initializeAllDegree();
     }
+        
+    public static void initializeAllDegree()
+    {
+    	for (int x_ind = 0; x_ind < board_edge_size; x_ind++)
+    	{
+    		for (int y_ind = 0; y_ind < board_edge_size; y_ind++)
+    		{
+        		int grid_fixed_degree = 0;
+        		int m_ind = getIndFromXY(x_ind, y_ind);
+        		
+        		for (int case_ind = 0; case_ind < num_cases; case_ind++)
+        		{
+        			int x_step = getMoveX(case_ind);
+        			int y_step = getMoveY(case_ind);
+        			if(withinBound(x_ind + x_step, y_ind + y_step))
+        			{
+        				grid_fixed_degree = grid_fixed_degree + 1;
+        			//	valid_to_search_cases[m_ind][case_ind] = true;
+        			}
+        		}
+        		fixed_degree[x_ind][y_ind] = grid_fixed_degree; 
+        		dynamics_degree[x_ind][y_ind] = grid_fixed_degree;
+            //	to_search_cases[m_ind] = 0;
+    		}
+    	}    	    	
+    	return ;
+    }
+
+    
+    ///////////////////////// index/case motion helper function ///////////////////////// 
     
     public static int getXFromInd(int ind)
     {
@@ -165,7 +174,30 @@ public class h1_draft00
 
     	return y;
     }
-        
+    
+    public static int sampleMove()
+    {
+    	int sample = rn.nextInt() % board_edge_size;
+    	assert((sample>=0) && (sample<board_edge_size));
+    	return sample;
+    }
+    
+    public static boolean inList(int move_case, int[] searched_list)
+    {
+    	boolean in_list = false;
+    	for (int case_id = 0; case_id < num_cases; case_id++)
+    	{
+    		if(move_case == searched_list[case_id])
+    		{
+    			return true;
+    		}
+    	}
+    	return in_list;
+    }
+
+    
+    /////////////////////////// validation helper function ///////////////////////
+    
     public static boolean withinBound(int x, int y)
     {
     	boolean valid = true;
@@ -234,18 +266,127 @@ public class h1_draft00
     	
         return false;
     }
-
-    public static int sampleMove()
-    {
-    	int sample = rn.nextInt() % board_edge_size;
-    	assert((sample>=0) && (sample<board_edge_size));
-    	return sample;
-    }
+    
         
+    /////////////////////////// heuristic, motion selection /////////////////////////
+    
+    public static int nextMoveByFixedDegree(int cur_x, int cur_y, int[] searched_list)
+    {
+    	int next_case_ind = -1;
+    	int min_num_cases = 9;
+    	int m_ind = getIndFromXY(cur_x, cur_y);
+    	
+    	for(int case_ind = 0; case_ind < num_cases; case_ind++)
+    	{
+    		int x_step = getMoveX(case_ind);
+			int y_step = getMoveY(case_ind);
+			// is_valid_move is true if withbound and not visited
+			boolean is_valid_move = testLMove(cur_x, cur_y, case_ind);
+			boolean is_searched = inList(case_ind, searched_list);
+
+			if (is_valid_move && (!is_searched))
+			{
+				int next_fixed_degree = fixed_degree[cur_x+x_step][cur_y+y_step];
+				if(next_fixed_degree < min_num_cases)
+				{
+					next_case_ind = case_ind;
+					min_num_cases = next_fixed_degree;
+				}
+			}
+    	}    	
+    	// return -1 if no valid move found;
+    	return next_case_ind;
+    }
+    
+    public static int nextMoveByDynamicsDegree(int cur_x, int cur_y)
+    {
+    	int next_case_ind = -1;
+    	int min_num_cases = 9;
+    	int m_ind = getIndFromXY(cur_x, cur_y);
+    	
+    	for(int case_ind = 0; case_ind < num_cases; case_ind++)
+    	{
+    		int x_step = getMoveX(case_ind);
+			int y_step = getMoveY(case_ind);
+			// is_valid_move is true if withbound and not visited
+			boolean is_valid_move = testLMove(cur_x, cur_y, case_ind);
+
+			if (is_valid_move)
+			{
+				int next_fixed_degree = dynamics_degree[cur_x+x_step][cur_y+y_step];
+				if(next_fixed_degree < min_num_cases)
+				{
+					next_case_ind = case_ind;
+					min_num_cases = next_fixed_degree;
+				}
+			}
+    	}    	
+    	// return -1 if no valid move found;
+    	return next_case_ind;    	
+    }
+    
+    ///////////////////////// backup function //////////////////////////
+    
+    // All the data from #reset_ind will be wiped, reset_ind >0;
+    public static void resetSol(int reset_ind)
+    {
+    	assert(reset_ind > 0);
+    	assert(reset_ind < board_size);
+    	for (int m_ind = reset_ind; m_ind < board_size; m_ind++)
+    	{    		
+    		int x_ind = tour_x_trace[m_ind];
+    		int y_ind = tour_y_trace[m_ind];
+    		
+    		if((tour_x_trace[m_ind]<0))
+    		{    			
+    			return ;
+    		}
+    		//for (int case_ind = 0; case_ind < num_cases; case_ind++)
+			//{
+			//	valid_to_search_cases[m_ind][case_ind] = true;				
+			//}
+    		visited_state[x_ind][y_ind] = false;    		
+    		tour_x_trace[m_ind] = -1;
+    		tour_y_trace[m_ind] = -1;
+    		to_search_cases[m_ind] = 0;
+    	}
+    	
+    	//cur_m_ind = reset_ind - 1;
+    	visited_state[start_x][start_y] = true;
+    	tour_x_trace[0] = start_x;
+    	tour_y_trace[0] = start_y;
+    }
+
+    public static void backDynamicsDegree(int out_square_id)
+    {
+    	for (int case_ind = 0; case_ind < num_cases; case_ind++)
+    	{
+			int x_out = tour_x_trace[out_square_id];
+    		int y_out = tour_y_trace[out_square_id];
+			// is_valid_move is true if withbound and not visited
+			boolean is_valid_move = testLMove(x_out, y_out, case_ind);
+			if (is_valid_move)
+			{
+	    		int x_neighbor = x_out + getMoveX(case_ind);
+				int y_neighbor = y_out + getMoveY(case_ind);
+			//	int ind_neighbor = getIndFromXY(x_neighbor, y_neighbor);
+				dynamics_degree[x_neighbor][y_neighbor] = dynamics_degree[x_neighbor][y_neighbor]+1;
+			}
+
+    	}
+    	return ;
+    }
+    
+    ///////////////////////////// strategy function ///////////////////////
+    
     // start_ind is the last node in the tour_x/y_trace, being explored
     // start_ind < board_size
     public static boolean runStrategy0 (int start_ind)
     {       	
+    	if(sol_count > max_sol_count)
+    	{
+    		return false;
+    	}
     	int cur_m_ind = start_ind;
     	int next_m_ind = cur_m_ind + 1;
 		int cur_x = tour_x_trace[cur_m_ind];
@@ -266,7 +407,7 @@ public class h1_draft00
 				return true;			
     		}
     		// printSol();
-    		// resetSol(cur_m_ind);
+    		resetSol(cur_m_ind);
     		return false;
     	}
     	
@@ -294,33 +435,160 @@ public class h1_draft00
     			
     			while(found_sol)
     			{
-				//if(next_m_ind < board_size -1)
-				//{
-				//	resetSol(next_m_ind + 1);
-				//}
     				found_sol = runStrategy0(next_m_ind);    				
     			}
     		    resetSol(next_m_ind);
-    		//	if(!found_sol)
-    		//	{
-    		//		resetSol(next_m_ind);
-    		//		return true;
-    		//	}
-    		//	else 
-    		//	{
-//    				resetSol(next_m_ind);
-    		//	}
-    			// if have explored all cases    			    				
     		}
 		}
 		// next time, should explore case_ind  + 1
 		to_search_cases[cur_m_ind] = 8;    			
 
-    	// resetSol(cur_m_ind);
+        resetSol(cur_m_ind);
     	return false;
     	
     }
 
+    public static boolean runStrategy1(int start_ind)
+    {
+    	if(sol_count > max_sol_count)
+    	{
+    		return false;
+    	}
+    	
+    	int cur_m_ind = start_ind;
+    	int next_m_ind = cur_m_ind + 1;
+		int cur_x = tour_x_trace[cur_m_ind];
+		int cur_y = tour_y_trace[cur_m_ind];
+		boolean cur_visited = isVisited(cur_x, cur_y);
+		assert(cur_visited);
+    	    	
+    	if (next_m_ind == board_size)
+    	{
+    		if((validLast(tour_x_trace[cur_m_ind], tour_y_trace[cur_m_ind])) && to_search_cases[cur_m_ind]<8)
+    		{
+    			to_search_cases[cur_m_ind] = 8;
+    			
+    			visited_state[cur_x][cur_y] = true;
+	    		printSol();
+	    		sol_count = sol_count+1;
+	    	//	resetSol(cur_m_ind);
+				return true;			
+    		}
+    		// printSol();
+    		resetSol(cur_m_ind);
+    		return false;
+    	}
+
+    	int[] searched_list = new int [num_cases];
+    	for(int case_id = 0; case_id < num_cases; case_id++)
+    	{
+    		searched_list[case_id] = -1;
+    	}
+
+    	int move_case = nextMoveByFixedDegree(cur_x, cur_y, searched_list);
+    	while(move_case >= 0)
+    	{			
+    		searched_list[move_case] = move_case;
+			int x_step = getMoveX(move_case);
+			int y_step = getMoveY(move_case);
+			tour_x_trace[next_m_ind] = x_step + cur_x;
+			tour_y_trace[next_m_ind] = y_step + cur_y;
+			visited_state[x_step + cur_x][y_step + cur_y] = true;
+			//valid_to_search_cases[cur_m_ind][move_case] = false;
+		   			
+			boolean found_sol = runStrategy1(next_m_ind);
+			
+			while(found_sol)
+			{
+				found_sol = runStrategy1(next_m_ind);    				
+			}
+		    resetSol(next_m_ind);
+		    move_case = nextMoveByFixedDegree(cur_x, cur_y, searched_list);
+    	}
+    	
+		resetSol(cur_m_ind);
+		return false;
+    	
+    }
+    
+    public static boolean runStrategy2(int start_ind)
+    {
+    	if(sol_count > max_sol_count)
+    	{
+    		return false;
+    	}
+    	
+    	int cur_m_ind = start_ind;
+    	int next_m_ind = cur_m_ind + 1;
+		int cur_x = tour_x_trace[cur_m_ind];
+		int cur_y = tour_y_trace[cur_m_ind];
+		boolean cur_visited = isVisited(cur_x, cur_y);
+		assert(cur_visited);
+    	    	
+    	if (next_m_ind == board_size)
+    	{
+    		if((validLast(tour_x_trace[cur_m_ind], tour_y_trace[cur_m_ind])) && to_search_cases[cur_m_ind]<8)
+    		{
+    			to_search_cases[cur_m_ind] = 8;
+    			
+    			visited_state[cur_x][cur_y] = true;
+	    		printSol();
+	    		sol_count = sol_count+1;
+	    	//	resetSol(cur_m_ind);
+				return true;			
+    		}
+    		// printSol();
+    		//backDynamicsDegree(cur_m_ind);
+    		// resetSol(cur_m_ind);
+    		return false;
+    	}
+
+    	int move_case = nextMoveByDynamicsDegree(cur_x, cur_y);
+    	while(move_case >= 0)
+    	{			
+    		int x_step = getMoveX(move_case);
+			int y_step = getMoveY(move_case);
+			int x_next = x_step + cur_x;
+			int y_next = y_step + cur_y;
+			tour_x_trace[next_m_ind] = x_next;
+			tour_y_trace[next_m_ind] = y_next;
+			visited_state[x_next][y_next] = true;
+			//valid_to_search_cases[cur_m_ind][move_case] = false;
+			
+			// decrease neighbor grid dynamic degree
+	    	for (int case_ind = 0; case_ind < num_cases; case_ind++)
+	    	{
+				// is_valid_move is true if withbound and not visited
+				boolean is_valid_move = testLMove(x_next, y_next, case_ind);
+				if (is_valid_move)
+				{
+		    		int x_neighbor = x_next + getMoveX(case_ind);
+					int y_neighbor = y_next + getMoveY(case_ind);
+				//	int ind_neighbor = getIndFromXY(x_neighbor, y_neighbor);
+					dynamics_degree[x_neighbor][y_neighbor] = dynamics_degree[x_neighbor][y_neighbor]-1;
+				}
+	    	}
+
+		   			
+			boolean found_sol = runStrategy1(next_m_ind);
+			
+			while(found_sol)
+			{
+				found_sol = runStrategy1(next_m_ind);    				
+			}
+			backDynamicsDegree(next_m_ind);
+		    resetSol(next_m_ind);
+		    move_case = nextMoveByDynamicsDegree(cur_x, cur_y);
+    	}
+    	
+    	backDynamicsDegree(cur_m_ind);
+		resetSol(cur_m_ind);
+		return false;
+    	
+    }
+    
+    ///////////////////////////// print helper /////////////////////////////
+    
     public static void printSol()
     {
     	// TODO: change the heading 
@@ -341,4 +609,5 @@ public class h1_draft00
 		System.out.print(start_y); 
     	System.out.print("\n");
     }
+
 }
--- HWK1/HWK_1_try/src/knight_plot_helper.m
+++ HWK1/HWK_1_try/src/knight_plot_helper.m
@@ -2,25 +2,30 @@ clear all
 close all
 %[x,y] = textread('out_cut.txt','%n %n', 'headerlines',2);
 
-num_skip = 0;
-start_ind = num_skip * 66;
+board_edge = 8;
+board_size = board_edge^2;
+
+num_skip = 1;
+start_ind = num_skip * (board_size + 2);
 
 fid = fopen('out.txt','rt');
-input = textscan(fid,'%d %d', 65, 'headerlines', start_ind + 2);
+input = textscan(fid,'%d %d', board_size+1, 'headerlines', start_ind + 2);
 x = cell2mat(input(:,1));
 y = cell2mat(input(:,2));
 
 x_start = 2;
 y_start = 3;
 
-x_end = x(64);
-y_end = y(64);
+x_end = x(board_size);
+y_end = y(board_size);
 
-show_ind = 1:64;
+show_ind = 1:board_size;
 figure
 plot(x(show_ind),y(show_ind),'r*-', ...
     x_start, y_start,'bo',...
     x_end, y_end,'bo')
+tickValues = 0:1:(board_edge-1);
+set(gca,'XTick',tickValues)
 grid on
 
 
